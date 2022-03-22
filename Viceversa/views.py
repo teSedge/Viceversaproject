f023ca7b5643a6e28f43b7a8605a2222f0ce277b
teSedge <teSedge99@gmail.com>
2022-03-22 15:43:24 +0300

    4.0

--- Viceversa/views.py
+++ Viceversa/views.py
@@ -8,7 +8,7 @@ pass
 def reverse(request):
 	user_text = request.GET['usertext']
 	reverse_text =user_text[::-1]
-	words_count = user_text.count(" ") + 1
+	words_count = len(user_text.split())
 	return render(request, 'reversed text.html', {'usertext': user_text, 'reversetext': reverse_text,\
 		'wordscount':words_count})
 pass
